994c6dbf8bc5943b238b00a6ae03c35e5b13c7bf
Chrysan <sunjuhua@outlook.com>
2022-03-07 17:55:38 +0800

    ElIconModules

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as ElIconModules from "@element-plus/icons-vue";
 
 createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App);
+for (const iconName in ElIconModules) {
+    if (Reflect.has(ElIconModules, iconName)) {
+      const item = ElIconModules[iconName];
+      app.component(iconName, item);
+    }
+  }
+app.use(ElementPlus, { size: 'small', locale: zhCn,zIndex: 3000 });
+app.use(store).use(router).mount('#app');
